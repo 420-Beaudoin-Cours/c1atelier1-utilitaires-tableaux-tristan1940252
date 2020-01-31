bf3e9004aa5f5550b0d445b433f3538d9b708f4a
tristan1940252 <trit0tv0@gmail.com>
2020-01-31 12:03:58 -0500

    Tanner

---
+++ TableauxTest.java
@@ -0,0 +1,32 @@
+package sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TableauxTest {
+
+    @Test
+    public void trierCroissantSSS() {
+    }
+
+    @Test
+    public void trierDecroissant() {
+    }
+
+    @Test
+    public void maximum() {
+    }
+
+    @Test
+    public void minimum() {
+    }
+
+    @Test
+    public void moyenne() {
+    }
+
+    @Test
+    public void fouilleSeq() {
+    }
+}
